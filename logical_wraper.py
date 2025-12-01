568c0f662d146bd5bdc2d172a925600eb77cd27a
JBT13 <jeremiahborjas@gmail.com>
2025-12-01 13:17:38 +0000

    layers

---
+++ logical_wraper.py
@@ -0,0 +1,24 @@
+from modules import Tournament, Event
+
+class LLWrapper:
+   def create_tournament():
+      pass
+   
+   def generate_schedule(tournament: Tournament, event: Event):
+      pass
+   
+   def enter_match_result():
+      pass
+   
+   def change_team_captain():
+      pass
+   
+   def create_player():
+      pass
+   
+   def assign_point():
+      pass
+   
+   def get_statistics(ptc: str): #PTC = Player Team Club
+      pass
+   
