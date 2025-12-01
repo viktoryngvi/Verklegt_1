c9ffc582a60460f90ad9ae29c487d8db28b09b41
JBT13 <jeremiahborjas@gmail.com>
2025-12-01 11:20:07 +0000

    Hello world

---
+++ modules.py
@@ -0,0 +1,28 @@
+class Person:
+    def __init__(self, name: str, phone: str, address: str, dob: str, email: str):
+        self.name = name
+        self.phone = phone
+        self.address = address
+        self.dob = dob
+        self.email = email
+
+class Organiser:
+    super()
+
+class Spectator:
+    super()
+
+class Player:
+    super()
+
+class Tournament:
+    def __init__(self):
+        pass
+
+class Match:
+    def __init__(self):
+        pass
+
+class Team:
+    def __init__(self):
+        pass
