b686a50e746486e07d184401a21dc4a0d803044e
JBT13 <jeremiahborjas@gmail.com>
2025-12-02 15:10:59 +0000

    working on playerll

--- src/LL/matchLL.py
+++ src/LL/matchLL.py
@@ -1,6 +1,9 @@
 class MatchLL:
-    def enter_match_result(team_a_score: int, team_b_score: int):
+    def __init__(self):
+        pass
+
+    def enter_match_result(self, team_a_score: int, team_b_score: int):
         if team_a_score < 0 and team_b_score < 0:
             return False
         
-        return f"{team_a_score} - {team_b_score}"
+        return f"{team_a_score}{team_b_score}"
