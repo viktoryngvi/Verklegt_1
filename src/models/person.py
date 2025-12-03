8fcc4837ae0b3b99807cf153f30128cc52a07175
JBT13 <jeremiahborjas@gmail.com>
2025-12-03 10:35:09 +0000

    changing models

---
+++ src/models/person.py
@@ -0,0 +1,7 @@
+class Person:
+    def __init__(self, name: str, phone: str, address: str, dob: str, email: str):
+        self.name = name
+        self.phone = phone
+        self.address = address
+        self.dob = dob
+        self.email = email
