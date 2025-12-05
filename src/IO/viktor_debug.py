56c199cd79a14e0df2f429ea1bb0df3faa3aa318
viktoryngvi <viktoryngvi@gmail.com>
2025-12-05 10:45:25 +0000

    update

--- src/IO/viktor_debug.py
+++ src/IO/viktor_debug.py
@@ -42,16 +42,42 @@ from csv import DictReader
 
 
 #load all player info()
-def check_if_player_exists():
+# def check_if_player_exists():
+#     with open("data/player_info.csv", "r", encoding="utf-8") as player_file:
+#         csv_reader = DictReader(player_file)
+#         player_list = list(csv_reader)
+#     return player_list
+
+
+# # check_if_player_exists()
+# player_list = check_if_player_exists()
+# list_of_names = []
+# for players in player_list:
+#     names = str(players["name"])
+#     list_of_names.append(names)
+
+
+
+
+# Edit player function
+def edit_player_function(find_player_id, what_to_edit):
     with open("data/player_info.csv", "r", encoding="utf-8") as player_file:
         csv_reader = DictReader(player_file)
         player_list = list(csv_reader)
-    return player_list
+        player_list
+
+    for player in player_list:
+        id = int(player["id"])
+        if find_player_id == id:
+            player_to_edit = player
+    return "player does not exist"
+    
+
+    with open ("data/player_info_new_test.csv", "w", encoding="utf-8") as player_file:
+        pl
 
 
-# check_if_player_exists()
-player_list = check_if_player_exists()
-list_of_names = []
-for players in player_list:
-    names = str(players["name"])
-    list_of_names.append(names)
+
+find_player_id = int(input("hvaða id viltu finna: "))
+finna_player = edit_player_function(find_player_id)
+print(finna_player)
