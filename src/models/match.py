531890ca18b46b32bbe4c02ca5bf8cd139294867
Arnór <arnorpostur@gmail.com>
2025-12-05 14:36:53 +0000

    unnið í event

--- src/models/match.py
+++ src/models/match.py
@@ -1,12 +1,13 @@
+
 from models.team import Team
 
 class Match:
-    def __init__(self, id: int, team_a: Team, team_b: Team, winner: Team, result_score: str, schedule_time: str, server_id: str ):
+    def __init__(self, id: int, team_a: Team, team_b: Team, winner: Team = None,
+                 schedule_time: str = "", server_id: str = ""):
+        
         self.id = id
         self.team_a = team_a
         self.team_b = team_b
-        self.winner = winner 
-        self.result_score = result_score
+        self.winner = winner
         self.schedule_time = schedule_time
         self.server_id = server_id
-        
