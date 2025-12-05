107174e6752ac852ecbd127ed8a3dc314978063b
Tristan608 <tristansd17@gmail.com>
2025-12-05 11:25:02 +0000

    s

--- src/UI/captainUI.py
+++ src/UI/captainUI.py
@@ -3,7 +3,7 @@ from models.player import Player
 
 
 class CaptainUI:
-    def __init__(self, ll_wrapper:LLWrapper, menu_ui):
+    def __init__(self, ll_wrapper, menu_ui):
         self.ll = ll_wrapper
         self.menu_ui = menu_ui
 
