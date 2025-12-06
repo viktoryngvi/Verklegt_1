dd552bcd29c6410541713d78f4bc73cf0e08734e
viktoryngvi <viktoryngvi@gmail.com>
2025-12-06 15:13:00 +0000

    laga load all player short info

--- src/IO/data_wrapper.py
+++ src/IO/data_wrapper.py
@@ -31,9 +31,11 @@ class DLWrapper:
         return self.playerio.load_all_player_info(player)
     
     def check_if_handle_exists_with_player(self, player : Player_IO):
-        return self.playerio.check_if_handle_exists(player)
+        return self.playerio.check_if_handle_exists_with_player(player)
     
-    def check_if_handle_exists_with_handle(self)
+    def check_if_handle_exists_with_handle(self, handle):
+        return self.playerio.check_if_handle_exists_with_handle(handle)
+
     
 
 
