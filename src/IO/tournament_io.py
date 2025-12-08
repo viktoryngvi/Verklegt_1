acf3ce6abeeeba96e407986fb7b643a0e18d032f
viktoryngvi <viktoryngvi@gmail.com>
2025-12-08 15:22:43 +0000

    ?

--- src/IO/tournament_io.py
+++ src/IO/tournament_io.py
@@ -1,17 +1,21 @@
 from models.tournament import Tournament
-from models.person import Person
-
-
+from csv import DictReader
 class Tournament_IO(Tournament):
 
     def __init__(self):
         self.file_path = "data/tournament.csv"
 
-    def 
+    def read_file_as_list_of_dicts(self):
+        with open(self.file_path, "r", encoding="utf-8") as Tournament_file:
+            csv_reader = DictReader(Tournament_file)
+            tournament_data = list(csv_reader)
+        return tournament_data
 
-    def create_tournament(self):
+    def create_tournament(self, name):
         """tournaments should have a list of tournaments that have event names"""
-        with open(self.file_path, "a", encoding="utf-8") as tournament_file
+        with open(self.file_path, "a", encoding="utf-8") as tournament_file:
+            
+
     
     def view_tournaments():
         pass
