a6c9695471435fa2ba9bd3a38e451763d81a8551
JBT13 <jeremiahborjas@gmail.com>
2025-12-09 10:56:10 +0000

    Club update

--- src/models/club.py
+++ src/models/club.py
@@ -1,9 +1,9 @@
-
+from models.team import Team
 class Club:
-    def __init__(self, name: str, home_town: str, country: str, colors: list):
+    def __init__(self, name: str, home_town: str, country: str, color: list, teams: list[Team]):
         self.name = name
         self.home_town = home_town
         self.country = country
-        self.colors = colors
-        pass
+        self.color = color
+        self.teams = teams
 
