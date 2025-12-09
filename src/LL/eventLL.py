64ef067eaab4f82a3baa1c58047c389e3483f2d9
JBT13 <jeremiahborjas@gmail.com>
2025-12-09 18:19:45 +0000

    event please?

--- src/LL/eventLL.py
+++ src/LL/eventLL.py
@@ -7,14 +7,17 @@ class EventLL:
         self.dl_wrapper = dl_wrapper
         self.validate = validate
     
-    def create_event(self, tournament_name, event: Event):
+    def create_empty_event(self, event: Event):
         validate_errors = self.validate.validate_event(event)
         if validate_errors:
             return validate_errors
+    
+    def write_team_into_event(self, event: Event):
+        self.dl_wrapper.create_empty_event(event.tournament_name)
+        self.dl_wrapper.write_team_into_empty_event(event.team)
         
+
         
-        
-        return self.dl_wrapper.create_event(tournament_name, event)
 
 
         
--- src/models/event.py
+++ src/models/event.py
@@ -4,7 +4,8 @@ from models.team import Team
 from models.match import Match
 
 class Event:
-    def __init__(self, name: str, game_type: str, start_date: str, end_date: str, teams: list[Team], matches: list[Match]):
+    def __init__(self, tournament_name: str,  name: str, game_type: str, start_date: str, end_date: str, teams: list[Team], matches: list[Match]):
+        self.tournament_name = tournament_name
         self.name = name 
         self.game_type = game_type
         self.start_date = start_date
