6ff81353f98da1e5ab762f6c6081b398aa07a674
JBT13 <jeremiahborjas@gmail.com>
2025-12-09 15:10:38 +0000

    match

--- src/LL/eventLL.py
+++ src/LL/eventLL.py
@@ -1,5 +1,5 @@
-
+from IO.data_wrapper import DLWrapper
 
 class EventLL:
-    def __init__(self):
-        pass
+    def __init__(self, dl_wrapper: DLWrapper):
+        self.dl_wrapper = dl_wrapper
