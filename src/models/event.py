66969d0aa5ffb2d474bee938f92d7b8648f43c5d
viktoryngvi <viktoryngvi@gmail.com>
2025-12-10 20:30:37 +0000

    lagfæring

--- src/models/event.py
+++ src/models/event.py
@@ -1,11 +1,11 @@
 # models/event.py
 
 from models.team import Team
-from models.match import Match
+# from models.match import Match
 
 class Event:
     def __init__(self, tournament_name: str = None, name :str = None, game_type: str = None, start_date: str = None,
-                  end_date: str = None, team: list[Team] = None, matches: list[Match] = None):
+                  end_date: str = None, team: list[Team] = None):
         
         self.tournament_name = tournament_name 
         self.name = name
--- src/models/match.py
+++ src/models/match.py
@@ -1,12 +1,14 @@
 from models.team import Team
+from models.event import Event
 
-class Match:
-    def __init__(self, id: int, teams: list[Team], result_score: str, winner: Team = None,
+class Match(Event):
+    def __init__(self, id: int = None, teams: list[Team], result_score: str, winner: Team = None,
                  schedule_time: str = "", server_id: str = ""):
-        
+        super().__init__(tournament_name = None, name = None)
         self.id = id
         self.teams = teams
         self.result_score = result_score
         self.winner = winner
         self.schedule_time = schedule_time
         self.server_id = server_id
+        self.tournament_name = tournament_name 
