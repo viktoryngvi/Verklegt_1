ef76f1ab5dd926f26995aba0fe3b0627a0e0abd2
JBT13 <jeremiahborjas@gmail.com>
2025-12-10 21:45:49 +0000

    hello world

--- src/models/team.py
+++ src/models/team.py
@@ -1,7 +1,7 @@
 from models.player import Player
 
 class Team:
-    def __init__(self, name: str = None, captain: Player.handle = None, players: list[Player] = None):
+    def __init__(self, name: str = None, captain: Player = None, players: list[Player] = None):
         self.name = name
         self.captain = captain
         self.players = players
