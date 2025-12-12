4a090f1f7072af5c915fccc90d5595c32fe99e0e
viktoryngvi <viktoryngvi@gmail.com>
2025-12-12 12:02:40 +0000

    event update

--- src/LL/eventLL.py
+++ src/LL/eventLL.py
@@ -31,7 +31,7 @@ class EventLL:
         
         if self.write_event_into_tournament(event):
 
-            return self._dl_wrapper.create_empty_event(event)
+            return self._dl_wrapper.create_empty_event_blueprint()
 
     # ----------------------------------------------------------------------
     # EVENT TYPES 
@@ -57,17 +57,6 @@ class EventLL:
     #             return useable_id
     
 
-
-# laga þetta
-    # def create_empty_event(self, event: Event):
-    #     self._dl_wrapper.write_into_event_blueprint('id,team_name,event_name,event_type,tournment_name,start_date,end_date')
-
-    #     for id in range(1,17):
-    #         self._dl_wrapper.write_into_event_blueprint(f'{id},"team",{event.name},{event.game_type},{event.tournament_name},{event.start_date},{event.end_date}\n')
-    #     return "Event created!"
-    
-# #############################################
-
     # ----------------------------------------------------------------------
     # WRITE EVENT INTO TOURNAMENT
     # ----------------------------------------------------------------------
@@ -90,7 +79,7 @@ class EventLL:
     def check_if_team_in_event(self, team):
         """CHECKS IF A TEAM IS ALREADY IN THE EVENT"""
     
-        event_data = self._dl_wrapper.load_all_player_info()
+        event_data: list[Event] = self._dl_wrapper.load_event_blueprint()
 
         for line in event_data:
             if line.team_name == team:
@@ -103,31 +92,30 @@ class EventLL:
     # MAKE EVENT PUBLIC
     # ----------------------------------------------------------------------
 
-    def make_event_public(self):
-        """MOVES EVENT DATA TO PUBLIC FILE OR KNOCKOUT"""
+    # def make_event_public(self):
+    #     """MOVES EVENT DATA TO PUBLIC FILE OR KNOCKOUT"""
     
-        blueprint_file = self._dl_wrapper.load_event_blueprint()
+    #     blueprint_file: list[Match] = self._dl_wrapper.load_event_blueprint()
     
-        for line in blueprint_file:
-            if line["id"] == 1:
-                type_of_event = line["event_type"]
+    #     for line in blueprint_file:
+    #         if line.event_id == 1:
+    #             type_of_event = line.game_type
     
-        if type_of_event == "knockout":
-            return self.move_blueprint_to_knockout()
+    #     if type_of_event == "knockout":
+    #         return self.move_blueprint_to_match_file()
     
-        if type_of_event == "last_team_standing":
-            return self._dl_wrapper.edit_match_file()
+    #     if type_of_event == "last_team_standing":
+    #         return self._dl_wrapper.edit_match_file()
  
     # ----------------------------------------------------------------------
-    # MOVE BLUEPRINT TO KNOCKOUT
+    # MOVE BLUEPRINT TO Match
     # ----------------------------------------------------------------------
  
-    def move_blueprint_to_knockout(self):
-        """CREATES KNOCKOUT STRUCTURE FROM EVENT BLUEPRINT"""
-    
-        knockout_file = self._dl_wrapper.move_to_knockout("tournament,event_name,game_type,server_id,match_id,bracket_nr,date_of_match,time_of_match,team_a,team_b,team_a_score,team_b_score,winner,match_result")
-    
-        self.create_first_round()
+    def move_blueprint_to_match_file(self):
+        """CREATES Match STRUCTURE FROM EVENT BLUEPRINT"""
+        
+        self._dl_wrapper.override_match_file()
+        self.create_first_rounds()
     
         return "Done!"
     
@@ -139,16 +127,12 @@ class EventLL:
         """GENERATES A UNIQUE SERVER ID"""
     
         return str(uuid.uuid4())
-
-        
-
-
     
 
-    # def get_match_id(self):
-    #     schedule_file = self.read_schedule_file_as_list_of_dict()
-    #     last_id = int(schedule_file[-1]["match_id"])
-    #     return last_id + 1
+    def get_match_id(self):
+        schedule_file: list[Match] = self._dl_wrapper.load_match_file()
+        last_id = int(schedule_file[-1].match_id)
+        return last_id + 1
 
     # ----------------------------------------------------------------------
     # INPUT MATCH RESULTS
@@ -181,24 +165,23 @@ class EventLL:
     # CREATE FIRST ROUND
     # ----------------------------------------------------------------------
   
-    def create_first_rounds(self, event: Event, match: Match):
+    def create_first_rounds(self):
         """CREATES FIRST ROUND MATCHUPS FOR TEAMS"""
-    
-        public_file: list[Team] = self._dl_wrapper.view_all_teams()
-    
+        public_file: list[Event] = self._dl_wrapper.view_all_teams()
         all_team_list = []
-    
         for team in public_file:
             all_team_list.append(team.name)
-    
+
+        bracket_id = 1
+
         for teamsvs in all_team_list:
             team_a = random.choice(teamsvs)
             all_team_list.remove(team_a)
             team_b = random.choice(teamsvs)
             all_team_list.remove(team_b)
             server_id = self.get_server_id()
-            bracket_id = self.get_bracket_id()
             ###############################Hvar eigum við að geyma server id??????? ég geri bara í match örsnöggt
+            self._dl_wrapper.append_to_match_file(match)
             self._dl_wrapper.append_to_match_file(f'{match.tournament_name},{match.event_name},{match.game_type},{match.match_id}{server_id},{bracket_id},{match.date_of_match}{match.time_of_match},{team_a},{team_b},team_a_score,team_b_score,winner,')
             
 
@@ -206,15 +189,15 @@ class EventLL:
     # GET BRACKET ID
     # ----------------------------------------------------------------------
  
-    def get_bracket_id(self, current_bracket):
+    def get_bracket_id(self):
         """DETERMINES NEXT BRACKET NUMBER"""
     
-        public_file: list[Match] = self._dl_wrapper.load_match_file()
+        match_file: list[Match] = self._dl_wrapper.load_match_file()
     
-        if not isinstance(public_file[-1].bracket_nr):
+        if not match_file[-1].bracket_nr:
             return 1
         else:
-            new_bracket_nr = public_file[-1].bracket_nr +1
+            new_bracket_nr = match_file[-1].bracket_nr + 1
             return new_bracket_nr
 
 
@@ -223,23 +206,25 @@ class EventLL:
     # CREATE ROUND 2, 3, AND 4
     # ----------------------------------------------------------------------
   
-    def create_round_2_3_and_4(self, round: int, match: Event):
+    def create_round_2_3_and_4(self, round_nr: int):
         """CREATES SUBSEQUENT ROUNDS BASED ON PREVIOUS WINNERS"""  
         
-        schedule_file = self._dl_wrapper.load_match_file()
-        
+        schedule_file: list[Match] = self._dl_wrapper.load_match_file()
         list_of_winners_from_bracket = []
         
-        for line in schedule_file[round:]:
-            list_of_winners_from_bracket.append(line["winner"])
+        for match in schedule_file[round_nr:]:
+            list_of_winners_from_bracket.append(match.winner)
+
+        bracket_id = self.get_bracket_id()
         
         for team in list_of_winners_from_bracket:
             team_a = list_of_winners_from_bracket[0]
             list_of_winners_from_bracket.pop(0)
             team_b = list_of_winners_from_bracket[0]
             list_of_winners_from_bracket.pop(0)
+
             server_id = self.get_server_id
-            self._dl_wrapper.move_to_knockout(f'{event.tournament_name},{event.name},{event.game_type},{server_id},{int(get_bracket_nr)},{date_of_match}{event.schedule_time},{team_a},{team_b},team_a_score,team_b_score,winner,')
+            self._dl_wrapper.append_to_match_file(f'{match.tournament_name},{match.event_name},{match.game_type},{server_id},{bracket_id},{match.date_of_match}{match.time_of_match},{team_a},{team_b},team_a_score,team_b_score,winner,')
         
         return "Round created"
             
@@ -250,9 +235,8 @@ class EventLL:
     def create_second_round(self):
         """CREATES SECOND ROUND MATCHUPS"""
         
-        round = int(0)
-        
-        second_round = self.create_round_2_3_and_4(round)
+        round_nr = int(0)
+        self.create_round_2_3_and_4(round_nr)
         
         return "Round created"
 
@@ -263,9 +247,9 @@ class EventLL:
     def create_third_round(self):
         """CREATES THIRD ROUND MATCHUPS"""
         
-        round = int(9)
+        round_nr = int(9)
         
-        third_round = self.create_round_2_3_and_4(round)
+        self.create_round_2_3_and_4(round_nr)
         
         return "Round created"
     
@@ -276,9 +260,9 @@ class EventLL:
     def create_fourth_round(self):
         """CREATES FOURTH ROUND MATCHUPS"""
         
-        round = int(13)
+        round_nr = int(13)
         
-        fourth_round = self.create_round_2_3_and_4(round)
+        self.create_round_2_3_and_4(round_nr)
         
         return "Round created"
 
@@ -286,14 +270,12 @@ class EventLL:
     # DECLARE WINNER
     # ----------------------------------------------------------------------
   
-    def declare_winner(self):
+    def find_winner(self):
         """RETURNS THE FINAL WINNER OF THE EVENT"""
         
-        knockout_file = self._dl_wrapper.load_match_file()
+        knockout_file: list[Match] = self._dl_wrapper.load_match_file()
         
-        for line in knockout_file[15:]:
-            winner = line["winner"] if line["winner"] else "No final winner yet"
-        
+        winner = knockout_file[-1].winner        
         return winner
 
     # ----------------------------------------------------------------------
@@ -303,12 +285,12 @@ class EventLL:
     def how_many_matches_have_winners(self):
         """COUNTS MATCHES THAT HAVE A WINNER"""
         
-        schedule_file = self._dl_wrapper.load_match_file
+        match_file: list[Match] = self._dl_wrapper.load_match_file()
         
         winners = 0
         
-        for line in schedule_file:
-            if line["winner"] != "winner":
+        for line in match_file:
+            if line.winner != "winner":
                 winners +=1
         
         return winners
